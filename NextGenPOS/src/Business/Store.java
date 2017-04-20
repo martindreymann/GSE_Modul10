e72e1403ee82a7cfe0952f6371d18b0a52313669
Martin Dreymann <martin@dreymann.dk>
2017-04-20 11:33:46 +0200

    xxx

--- NextGenPOS/src/Business/Store.java
+++ NextGenPOS/src/Business/Store.java
@@ -4,12 +4,13 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
 import java.util.TreeSet;
+import nextgenpos.IBusiness;
 
-public class Store {
+public class Store implements IBusiness {
 
     private String address, name;
     private ProductCatalog catalog;
-    private Map<String,Register> registers;
+    private Map<String, Register> registers;
     private Set<Sale> sales;
 
     public Store(String name, String address) {
@@ -24,18 +25,26 @@ public class Store {
         sales.addAll(register.getAllSales());
     }
 
+    @Override
+    public ProductDescription findProductDescription(String id) {
+        return catalog.getProductDescription(id);
+    }
+
+    @Override
+    public Map<String, Register> getRegisters() {
+        return registers;
+    }
+
     public void createNewProduct(String itemID, String description, float price) {
         catalog.createNewProduct(itemID, description, price);
     }
-    
-    public void createNewRegister(String name){
-        registers.put(name,new Register(name,catalog));
+
+    public void createNewRegister(String name) {
+        registers.put(name, new Register(name, catalog));
     }
-    
-    public Register getRegister(String name){
+
+    public Register getRegister(String name) {
         return registers.get(name);
     }
-    
-    
 
 }
--- NextgenPOS/src/nextgenpos/IBusiness.java
+++ NextgenPOS/src/nextgenpos/IBusiness.java
@@ -1,8 +1,13 @@
-
 package nextgenpos;
 
-
+import Business.ProductDescription;
+import Business.Register;
+import java.util.Map;
 
 public interface IBusiness {
+    
+    public abstract ProductDescription findProductDescription(String id);
+    public abstract Map<String,Register> getRegisters();
+    
     
 }
