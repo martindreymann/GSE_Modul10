741d7f135e08d92745149e6a2c01b5a624e0194b
FCBfreak <johan.jakobsen@hotmail.com>
2017-04-20 09:20:58 +0200

    Sale class

--- NextGenPOS/src/Business/Sale.java
+++ NextGenPOS/src/Business/Sale.java
@@ -1,5 +1,31 @@
 package Business;
 
+import java.util.ArrayList;
+import java.util.Date;
+
 public class Sale {
+    private Date date;
+    private boolean isComplete = false;
+    private ArrayList<Sale> salesLineItems = new ArrayList<>();
+
+    public Sale() {
+        date = new Date();
+    }
+    public void makeLineItem(){
+        
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public ArrayList<Sale> getSalesLineItems() {
+        return salesLineItems;
+    }
+
+    public void setIsComplete(boolean isComplete) {
+        this.isComplete = isComplete;
+    }
+    
     
 }
