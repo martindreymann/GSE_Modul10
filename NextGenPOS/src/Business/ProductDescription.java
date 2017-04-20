d2314629d0b60b03a52c0fd5d8e0fbfdf6068f7f
Martin Dreymann <martin@dreymann.dk>
2017-04-20 09:21:32 +0200

    ProductDescription stuff

--- NextGenPOS/src/Business/ProductDescription.java
+++ NextGenPOS/src/Business/ProductDescription.java
@@ -2,4 +2,25 @@ package Business;
 
 public class ProductDescription {
     
+    private String itemID, description;
+    private float price;
+    
+    public ProductDescription(String itemID, String description, float price){
+        this.itemID = itemID;
+        this.description = description;
+        this.price = price;
+    }
+
+    public String getItemID() {
+        return itemID;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+    
 }
