ef4e2626be6600b6c601e896d5c1fc1f179dbf1e
FCBfreak <johan.jakobsen@hotmail.com>
2017-04-20 09:10:56 +0200

    Payment Class

--- NextGenPOS/src/Business/Payment.java
+++ NextGenPOS/src/Business/Payment.java
@@ -1,5 +1,14 @@
 package Business;
 
 public class Payment {
+        private float amount;
+
+    public Payment(float amount) {
+        this.amount = amount;
+    }
+
+    public float getAmount() {
+        return amount;
+    }
     
 }
