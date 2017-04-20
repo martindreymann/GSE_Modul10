494cbe422b27896509f54e34955d442ccccf155e
Martin Dreymann <martin@dreymann.dk>
2017-04-20 09:00:19 +0200

    Store added

--- NextGenPOS/src/Business/Store.java
+++ NextGenPOS/src/Business/Store.java
@@ -3,4 +3,11 @@ package Business;
 
 public class Store {
     
+    private String address, name;
+
+    public Store(String name, String address){
+        this.name = name;
+        this.address = address;
+        
+    }
 }
